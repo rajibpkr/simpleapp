be1c512798b2b48824f6d34b103799d5d643dca3
rajibpkr <btcnepal@gmail.com>
2022-01-25 12:17:09 +0545

    jan25_12.16

--- simpleapp/urls.py
+++ simpleapp/urls.py
@@ -6,7 +6,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
-    path('about/',views.about ),  #first is route path and second is page
+    path('about/',views.about),  #first is route path and second is page
+    path('mypage/',views.mypage),  #first is route path and second is page
     path('', views.homepage),    #first is route path and second is page
     
 ]
--- simpleapp/views.py
+++ simpleapp/views.py
@@ -10,6 +10,22 @@ def homepage(request):  #when user visits this about url then fires this functio
     # return HttpResponse("This is Home page")
     return render(request, "homepage.html") #always follows with requests at first.
 
+def mypage(request):
+    return render(request, "my_page.html")
+
+def mypage(request):
+    data = {
+        'title':'Home New',
+        'bdata':'Welcome to WS Cube',
+        'clist':['PHP', 'JAVA', 'Django'],
+        'student_details':[
+            {'name':'pradeep', 'phone':9856203598,},
+            {'name':'testing', 'phone':9852059546},
+        ]
+
+    }
+    return render(request, "my_page.html", data)
+
 
 
     
